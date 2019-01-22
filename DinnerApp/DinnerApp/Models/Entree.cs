52ec97c9c5b3c6b1312c8825627046f48c42f21a
Charis Prasoulas <xprasoulas@gmail.com>
2019-01-22 20:39:29 +0200

    Create/Fill Class Entree/Ingredient

---
+++ DinnerApp/DinnerApp/Models/Entree.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace DinnerApp.Models
+{
+    public class Entree
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public List<Ingredient> Ingredients { get; set; }
+
+        public Entree()
+        {
+            Ingredients = new List<Ingredient>();
+        }
+    }
+}
